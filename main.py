03edfb2a035af6436f49c61792ed767b9ce79eca
Dmitry Meleshko <1dayac@gmail.com>
2021-12-12 10:19:01 -0500

    Fix stdout

--- main.py
+++ main.py
@@ -31,11 +31,10 @@ def run():
 
     for position in positions:
         for filename, name in all_datasets:
-            bash_command_view_2 = filename + " " + "\"" + position[0] + ":" + str(position[1] - 10000) + "-" + str(position[2] + 10000) + "\" " + \
-                ">" + os.path.join(final_dir, position[0] + "-" + str(position[1]) + "-" + str(position[2])  + "_" + name + ".bam")
+            bash_command_view_2 = filename + " " + "\"" + position[0] + ":" + str(position[1] - 10000) + "-" + str(position[2] + 10000) + "\" "
             full_view_command = bash_command_view_1 + bash_command_view_2
             print(full_view_command)
-            subprocess.call(full_view_command.split())
+            subprocess.call(full_view_command.split(), stdout=os.path.join(final_dir, position[0] + "-" + str(position[1]) + "-" + str(position[2])  + "_" + name + ".bam"))
             full_index_command = bash_command_index_1 + os.path.join(final_dir, position[0] + "-" + str(position[1]) + "-" + str(position[2])  + "_" + name + ".bam")
             subprocess.call(full_index_command.split())
 
